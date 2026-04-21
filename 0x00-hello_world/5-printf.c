ebf5e49721a67c710b57901c3afd0d06def3e56b
chukwudi sunday <chukwudis93@users.noreply.github.com>
2026-04-21 14:27:28 +0100

    Add program that prints message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/**
+*Author: Sunday Chukwudi
+*Program: WinMingle Community C Training
+*Description: Print a message using printf
+**/
+
+#include <stdio.h>
+
+/**
+*main - Entry poit of the program
+*Return: 0
+**/
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
