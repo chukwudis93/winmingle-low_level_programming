a19b2ece235664687aa2da4c7f8dc4d0b311958f
chukwudi sunday <chukwudis93@users.noreply.github.com>
2026-04-21 14:04:27 +0100

    Add program that prints message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+
+/**
+*Author: Sunday Chukwudi
+*program: WinMingle Community C Training
+*Description: Print a massage using puts
+**/
+
+#include <stdio.h>
+
+/**
+*main - Entry of the program
+*Return: 0 on success
+**/
+int main(void)
+{
+    puts("Programming is like building puzzle");
+    return (0);
+
+}
